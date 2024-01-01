c527efc5e1efd6176c9361cf3199180182bb6a65
siddiquenaim <naim.niter.tex06@gmail.com>
2024-01-01 15:31:36 +0600

    Added delete operation

--- app/components/ToDoList.jsx
+++ app/components/ToDoList.jsx
@@ -4,11 +4,49 @@ import { useEffect, useState } from "react";
 import { TiEdit, TiTick, TiTrash } from "react-icons/ti";
 
 const ToDoList = () => {
+  const [updated, setUpdated] = useState(false);
   const [existingTasks, setExistingTasks] = useState([]);
   useEffect(() => {
     const newExistingTasks = JSON.parse(localStorage.getItem("tasks")) || [];
     setExistingTasks(newExistingTasks);
-  }, [existingTasks]);
+  }, [updated]);
+
+  // delete task
+
+  const handleDeleteTask = (id) => {
+    const newTaskList = existingTasks.filter((task) => task?.id !== id);
+    localStorage.setItem("tasks", JSON.stringify(newTaskList));
+    setExistingTasks(newTaskList);
+    setUpdated(!updated);
+  };
+
+  // mark as completed
+
+  const handleMarkCompleted = (id) => {
+    const selectedTask = existingTasks.find((task) => task?.id === id);
+
+    if (selectedTask) {
+      selectedTask.isCompleted = true;
+
+      //create a new array that doesn't include the newly completed task
+      const newTaskList = existingTasks.filter((task) => task?.id !== id);
+
+      //updating the list using spread operator
+      const updatedList = [...newTaskList, selectedTask];
+      setExistingTasks(updatedList);
+      localStorage.setItem("tasks", JSON.stringify(updatedList));
+      setUpdated(!updated);
+    }
+  };
+
+  //update task name
+  const handleEditTask = (id) => {
+    // const selectedTask = existingTasks?.find((task) => task?.id === id);
+    // const newTaskList = existingTasks?.filter((task) => task?.id !== id);
+    // if(selectedTask){
+    //   // selectedTask.name = newname
+    // }
+  };
 
   return (
     <div>
@@ -32,18 +70,35 @@ const ToDoList = () => {
                 <tr key={singleTask?.id} className="text-center">
                   <td>
                     {singleTask?.taskName}{" "}
-                    <TiEdit className="text-2xl mx-auto text-green-600 inline-block cursor-pointer" />
+                    <TiEdit
+                      onClick={() => {
+                        handleEditTask(singleTask?.id);
+                      }}
+                      className="text-2xl mx-auto text-green-600 inline-block cursor-pointer"
+                    />
                   </td>
                   <td>{singleTask?.isCompleted ? "Completed" : "Pending"}</td>
                   <td>
                     {singleTask?.isCompleted ? (
                       <TiTick className="text-2xl mx-auto text-green-600" />
                     ) : (
-                      <button className="btn btn-sm">Mark Completed</button>
+                      <button
+                        onClick={() => {
+                          handleMarkCompleted(singleTask?.id);
+                        }}
+                        className="btn btn-sm"
+                      >
+                        Mark Completed
+                      </button>
                     )}
                   </td>
                   <td>
-                    <TiTrash className="text-2xl mx-auto text-red-600 cursor-pointer" />
+                    <TiTrash
+                      onClick={() => {
+                        handleDeleteTask(singleTask?.id);
+                      }}
+                      className="text-2xl mx-auto text-red-600 cursor-pointer"
+                    />
                   </td>
                 </tr>
               ))}
--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="p-24 text-center max-w-4xl mx-auto space-y-5">
       <div className="space-y-3">
-        <h1 className="text-2xl">To-do Application</h1>
+        <h1 className="text-2xl mb-3">To-do Application</h1>
         <AddTask />
       </div>
       <ToDoList />
